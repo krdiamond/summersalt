d16fff6851e3e469a28a8b6588d8df35d0a5d645
Kristina Diamond <krdiamond@gmail.com>
2023-04-23 22:13:08 -0400

    done but need to review

--- pages/index.js
+++ pages/index.js
@@ -1,126 +1,76 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import React, { useEffect, useState, useRef } from 'react';
 
 export default function Home() {
 
-  const handleClick = () => {
-    console.log('Button clicked!');
-  };
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await fetch(`https://summersalt.com/collections/swimwear/products.json?page=${page}&limit=6`);
+      const data = await response.json();
+      setProducts((prevProducts) => [...prevProducts, ...data.products]);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = containerRef.current;
+      const { scrollTop, scrollHeight, clientHeight } = container;
+
+      if (scrollHeight - scrollTop === clientHeight && !loading) {
+        console.log('Fetching next page...');
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    const container = containerRef.current;
+    container.addEventListener('scroll', handleScroll);
+
+    return () => {
+      container.removeEventListener('scroll', handleScroll);
+    };
+  }, [loading]);
+
 
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Summersalt Code Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Summersalt Code Challenge
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3>Hello World!</h3>
-            <button onClick={handleClick}>Click me!</button>
+        <div ref={containerRef}  className={styles.scroll_grid}>
+          <div className={styles.flex}>
+            {products.map((product) => (
+              <div key={product.id} className={styles.card}>
+                <h2><a href={`https://www.summersalt.com/products/${product.handle}`}>{product.title}</a></h2>
+                <a href={`https://www.summersalt.com/products/${product.handle}`}><img className={styles.product_image} src={product.images[0].src} alt={product.title} /></a>
+              </div>
+            ))}
           </div>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {loading && <p>Loading...</p>}
         </div>
+
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+       
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
